f3314f0092f8c734d5f21e1210898c692902b13a
Crystal <0830david@gmail.com>
2023-08-17 15:57:22 +0800

    HTML

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,26 @@
 // place files you want to import through the `$lib` alias in this folder.
+export const API_RESULT_DEFAULT = {
+    "vocab": "Vocabulary",
+    "pos": "Noun",
+    "definitions": [
+        {
+            "definition": "A usually alphabetized and explained collection of words e.g. of a particular field, or prepared for a specific purpose, often for learning."
+        },
+        {
+            "definition": "he collection of words a person knows and uses.",
+            "example" : "My Russian vocabulary is very limited."
+        },
+        {
+            "definition": "The stock of words used in a particular field.",
+            "example": "The vocabulary of social sciences is often incomprehensible to ordinary people."
+
+        },
+        {
+            "definition": "The words of a language collectively.",
+            "example": "The vocabulary of any language is influenced by contacts with other cultures."
+        }
+    ]
+}
+
+export const EXAMPLE_NOTFOUND = "This definition currently has no example."
+export const DICTS = ["Dictionary API", "Urban Dictionary", "Urban Dictionary", "Urban Dictionary"]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { API_RESULT_DEFAULT, EXAMPLE_NOTFOUND, DICTS } from "$lib"
+    import FaSearch from 'svelte-icons/fa/FaSearch.svelte'
+    let vocabInput = ""
+    let dictAPIResult1 = API_RESULT_DEFAULT
+</script>
+
+<div id="mainPage">
+    <div id="header">
+        <img class="icon" src="/vocabo.png" alt="Vocabo"/>
+        <input class="serachInput" bind:value={vocabInput} placeholder="Search: What is the vocab you want to learn today"/>
+        <FaSearch class="serachIcon"/>
+    </div>
+    <div class="dictionary" id="dict1">
+        <div class="label">{DICTS[0]}</div>
+        <h3 class="vocab">{dictAPIResult1.vocab || "Vocabulary"}</h3>
+        <div class="pos">{dictAPIResult1.pos || ""}</div>
+        {#each dictAPIResult1.definitions as defblock}
+            <div class="defblock">
+                <p class="def">{defblock.definition}</p>
+                <p class="exp" class:notfound={defblock.example == null}>{defblock.example || EXAMPLE_NOTFOUND}</p>
+            </div>
+        {/each}     
+        <div class="readmore">
+            <p>Read More</p>
+        </div>
+    </div>
+    <div class="dictionary" id="dict2">
+        <div class="label">{DICTS[1]}</div>
+        <div class="readmore">
+            <p>Read More</p>
+        </div>
+    </div>
+    <div class="dictionary" id="dict3">
+        <div class="label">{DICTS[2]}</div>
+        <div class="readmore">
+            <p>Read More</p>
+        </div>
+    </div>
+    <div class="dictionary" id="dict4">
+        <div class="label">{DICTS[3]}</div>
+        <div class="readmore">
+            <p>Read More</p>
+        </div>
+    </div>
+</div>
+
+<style>
+
+</style>
